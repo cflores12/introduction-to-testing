a11acd0b226b93a633f1c73f62636f22bba3e59f
chanales flores <cflores12@users.noreply.github.com>
2024-12-21 17:40:27 -0500

    test equality

--- examples/characters/src/character.test.ts
+++ examples/characters/src/character.test.ts
@@ -1,14 +1,48 @@
 import { describe, it, expect } from 'vitest';
 import { Character } from './character.js';
-import { Person } from './person.js';
 
 describe('Character', () => {
-  it.todo(
-    'should create a character with a first name, last name, and role',
-    () => {},
-  );
+  it('should create a character with a first name, last name, and role', () => {
+    const firstName = 'Grace';
+    const lastName = 'Hopper';
+    const role = 'SwordMaster';
+    const character = new Character(firstName, lastName, role);
+    /* expect(character).toEqual({
+      charisma: expect.anything(),
+      constitution: expect.anything(),
+      createdAt: expect.anything(),
+      dexterity: expect.anything(),
+      firstName: 'Grace',
+      id: expect.stringContaining('person-'),
+      intelligence: expect.anything(),
+      lastModified: expect.anything(),
+      lastName: 'Hopper',
+      level: 1,
+      role: 'SwordMaster',
+      strength: expect.anything(),
+      wisdom: expect.anything()
+      }); */
 
-  it.todo('should allow you to increase the level', () => {});
+      expect(character).toEqual(
+        expect.objectContaining({
+          firstName,
+          lastName,
+          role,
+        }),
+      );
+    });
 
-  it.todo('should update the last modified date when leveling up', () => {});
+  it('should allow you to increase the level', () => {
+    var character = new Character('Grace', 'Hopper', 'SwordMaster');
+    var initialLevel = character.level;
+    character.levelUp();
+    expect(character.level).toBeGreaterThan(initialLevel);
+  });
+
+  it('should update the last modified date when leveling up', () => {
+    var character = new Character('Grace', 'Hopper', 'SwordMaster');
+    var lastModDate = character.lastModDate;
+    character.levelUp();
+    expect(lastModDate).not.toEqual(character.lastModified);
+  });
 });
