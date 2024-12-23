2e3235a15b392e7ddf0f4f89177a6c044a006d38
chanales flores <cflores12@users.noreply.github.com>
2024-12-22 19:43:11 -0500

    testing dom

--- examples/accident-counter/src/counter.test.jsx
+++ examples/accident-counter/src/counter.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { Counter } from './counter';
@@ -10,18 +10,44 @@ describe.todo('Counter ', () => {
     render(<Counter />);
   });
 
-  it('renders with an initial count of 0');
+  it('renders with an initial count of 0', () => {
+    const counter = screen.getByTestId('counter-count');
+    expect(counter).toHaveTextContent('0');
+  });
 
-  it('disables the "Decrement" and "Reset" buttons when the count is 0');
+  it('disables the "Decrement" and "Reset" buttons when the count is 0', () => {
+    const decrementButon = screen.getByRole('button', { name : /decremenet/i });
+    const resetButton = screen.getByRole('button', { name : /reset/i });
 
-  it.todo('displays "days" when the count is 0', () => {});
+    expect(decrementButon).toBeDisabled();
+    expect(resetButton).toBeDisabled();
+  });
 
-  it.todo(
+  it('displays "days" when the count is 0', () => {
+    const title = screen.getByRole('title', { name : /day/i });
+    expect(title).toHaveTextContent('days');
+  });
+
+  it(
     'increments the count when the "Increment" button is clicked',
-    async () => {},
+    async () => {
+      const incremenetButton = screen.getByRole('button', { name: /incremenet/i});
+      const count = screen.getByTestId('counter-count');
+      //fireEvent(incremenetButton, new MouseEvent('click'));
+      //await userEvent.click(incremenetButton);
+      await act(async () => {
+        await userEvent.click(incremenetButton);
+      });
+      expect(count).toHaveTextContent('1');
+    },
   );
 
-  it.todo('displays "day" when the count is 1', async () => {});
+  it('displays "day" when the count is 1', async () => {
+    const incremenetButton = screen.getByRole('button', { name: /incremenet/i});
+    fireEvent(incremenetButton, new MouseEvent('click'));
+    const title = screen.getByRole('title', { name : /day/i });
+    expect(title).toHaveTextContent('day');
+  });
 
   it.todo(
     'decrements the count when the "Decrement" button is clicked',
--- examples/element-factory/src/button.test.js
+++ examples/element-factory/src/button.test.js
@@ -1,9 +1,33 @@
+import { fireEvent, screen } from '@testing-library/dom';
 import { createButton } from './button.js';
 
-describe.todo('createButton', () => {
-  it('should create a button element', () => {});
+describe('createButton', () => {
+  beforeEach(() => {
+    document.innerHTML = '';
+  });
 
-  it('should have the text "Click Me"', () => {});
+  it.skip('should create a button element', () => {
+    //const button = createButton();
+    document.body.appendChild(createButton());
+    const button = screen.getByRole('button', { name : 'Click Me'});
+    expect(button).toBeInTheDocument();
+   /*  expect(button).toBeInstanceOf(HTMLButtonElement);
+    expect(button.tagName).toBe('BUTTON'); */
+  });
 
-  it('should change the text to "Clicked!" when clicked', async () => {});
+  /* it('should have the text "Click Me"', () => {
+    const button = createButton();
+    expect(button.textContent).toBe('Click Me');
+  }); */
+
+  it('should change the text to "Clicked!" when clicked', async () => {
+    //const button = createButton();
+    document.body.appendChild(createButton());
+    const button = screen.getByRole('button', { name: 'Click Me'});
+    fireEvent(button, new MouseEvent('click'));
+    expect(button.textContent).toBe('Clicked!');
+    /* button.click();
+    expect(button.textContent).toBe('Clicked!'); */
+  });
+
 });
